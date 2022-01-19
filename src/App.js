d8e1cddda8e9f32b6dd56b33c4cdb8106ab25937
daokieu <kieudao0603@gmail.com>
2022-01-19 09:42:07 +0700

    outing add todos

--- src/App.js
+++ src/App.js
@@ -11,9 +11,23 @@ const  App = () => {
   // const name = 'daokieu';
   let [name, setName] = useState('daokieu');
   const [address, setAddress] = useState('');
+  const [todos, setTodos] = useState([
+    { id:'todo1', title:'watching hoi dan it channel' },
+    { id:'todo2',title: 'Doing homework' },
+    { id:'todo3',title: 'playing game' }
+  ]);
+
+
   const handleEventClick = (event) => {
-    setName(address)
-    console.log(">>>> click me", address)
+    if(!address) {
+      alert('emtpy input')
+      return;
+    }
+    
+    // hook not merge state
+    let newTodo = { id:'abc', title: address }
+    setTodos([...todos, newTodo])
+    setAddress('')
   }
   const handleInput = (event) => {
     setAddress(event.target.value)
@@ -28,8 +42,15 @@ const  App = () => {
       
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-          <h1>Hello world width 88 in { name } ! </h1>
-          <input type="text" value={ address } onChange = { (event) => handleInput(event) } ></input>
+        <h1>Hello world width 88 in { name } ! </h1>
+        <div className='todos-container'>
+          { todos.map(todo => {
+            return (
+              <li className='todo-child' key={ todo.id }> { todo.title } </li>
+            )
+          })}
+        </div>
+        <input type="text" value={ address } onChange = { (event) => handleInput(event) } ></input>
         <button  type='button' onClick={ () => handleEventClick() }>Click me</button> 
         
       </header>
